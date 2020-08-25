e26daabd69a3fa9a7a054438d1381ab7054e3885
Lelka <k.lelka@yahoo.fr>
2020-08-25 15:43:26 +0200

    Reorganized groups

--- TicTacToe/TicTacToe/Views/GameViewController.swift
+++ TicTacToe/TicTacToe/Views/GameViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var viewModel = GameViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 }
 
